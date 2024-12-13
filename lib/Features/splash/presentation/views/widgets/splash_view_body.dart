2c5a2bd639b273a0c12d2ce04b9e4ccaf0b1a538
kariMMeshal <kareemkhdh543@gmail.com>
2024-12-13 18:06:08 +0200

    Added Sliding Animation to Splash Screen

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,13 +1,53 @@
 import 'package:bookly_app/Core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(AssetsData.logoPath),
-    );
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(AssetsData.logoPath),
+          AnimatedBuilder(
+            animation: slidingAnimation,
+            builder: (context, child) => SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                "Read Free Books",
+                textAlign: TextAlign.center,
+              ),
+            ),
+          )
+        ]);
   }
 }
