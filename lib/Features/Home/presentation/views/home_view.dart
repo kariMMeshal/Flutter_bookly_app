3495b5938f1cf9a1df1cee40522311300c1ecada
kariMMeshal <kareemkhdh543@gmail.com>
2024-12-13 20:05:24 +0200

    added custom app bar

--- lib/Features/Home/presentation/views/home_view.dart
+++ lib/Features/Home/presentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +6,12 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Column(
+        children: [
+          CustomAppBar(),
+        ],
+      ),
+    );
   }
 }
