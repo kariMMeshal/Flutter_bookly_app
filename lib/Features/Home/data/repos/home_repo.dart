9a2d7f1bec3dace3c7c5696a56281e5d6779b7db
kariMMeshal <kareemkhdh543@gmail.com>
2024-12-16 15:40:16 +0200

    empelemnt  fetch newest books

--- lib/Features/Home/data/repos/home_repo.dart
+++ lib/Features/Home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly_app/Features/Home/data/models/book_model/book_model.dart'
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks();
   Future<Either<Failures, List<BookModel>>> featchFeaturedBooks();
 }
--- lib/Features/Home/data/repos/home_repo_implemntation.dart
+++ lib/Features/Home/data/repos/home_repo_implemntation.dart
@@ -1,20 +1,34 @@
-
-
 import 'package:bookly_app/Core/errors/failures.dart';
+import 'package:bookly_app/Core/utils/api_services.dart';
 import 'package:bookly_app/Features/Home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/Home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImplemntation extends HomeRepo{
+class HomeRepoImplemntation extends HomeRepo {
+  final ApiServices apiServices;
+
+  HomeRepoImplemntation(this.apiServices);
+
   @override
-  Future<Either<Failures, List<BookModel>>> featchFeaturedBooks() {
-    // TODO: implement featchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&q=subject:programming&Sorting=newest");
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(SeverFailure());
+    }
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
+  Future<Either<Failures, List<BookModel>>> featchFeaturedBooks() {
+    // TODO: implement featchFeaturedBooks
     throw UnimplementedError();
   }
 }
