8742b666624af0d1d356a6e89deffcf20a65f6e8
kariMMeshal <kareemkhdh543@gmail.com>
2024-12-16 17:40:52 +0200

    implement fetchFeaturedBooks

--- lib/Features/Home/data/repos/home_repo_implemntation.dart
+++ lib/Features/Home/data/repos/home_repo_implemntation.dart
@@ -31,8 +31,22 @@ class HomeRepoImplemntation extends HomeRepo {
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> featchFeaturedBooks() {
-    // TODO: implement featchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks() async{
+    try {
+      var data = await apiServices.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&q=subject:programming");
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
